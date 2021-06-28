74a5e23f81bc789f703bb328014e9fe2a94cf13d
happiness96 <daewonkim.mate@gmail.com>
2021-06-28 16:01:08 +0900

    21964 선린인터넷고등학교 교가

---
+++ Baekjoon_Online_Judge/NO_21964/21964.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+# -------------------------------------- #
+# | Created by happiness96             | #
+# | Year 2021                          | #
+# | Month 06                           | #
+# | Day 26                             | #
+# | 21964 선린인터넷고등학교 교가        | #
+# -------------------------------------- #
+
+
+if __name__ == "__main__":
+    N = int(r_input())
+
+    S = r_input().rstrip()
+
+    print(S[-5:])
